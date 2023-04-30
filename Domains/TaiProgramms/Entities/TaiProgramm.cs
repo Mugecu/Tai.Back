cd8a89e17b2f59ea5c946f52d0a053d98f949fde
Mugecu <forwork_ivan@tut.by>
2023-04-30 21:56:36 +0300

    доработан класс Программ

--- Domains/TaiProgramms/Entities/TaiProgramm.cs
+++ Domains/TaiProgramms/Entities/TaiProgramm.cs
@@ -8,5 +8,30 @@ namespace TaiProgramms.Entities
         public ShortDescription ShortDescription { get; private set; }
         public Description Description { get; private set; }
 
+        public TaiProgramm(Guid id, Title title, ShortDescription shortDescription, Description description)
+        {
+            Id = id;
+            Title = title;
+            ShortDescription = shortDescription;
+            Description = description;
+        }
+
+        public void ChangeProgrammName(string title)
+            => Title = new Title(title);
+
+        public void ChangeShortDescription(string shortDescription)
+            => ShortDescription = new ShortDescription(shortDescription);
+
+        public void ChangeDescription(string description)
+            => Description = new Description(description);
+
+        // добавить монаду Options или MayBe. 
+        public TaiProgramm UpdateProgramm(string title, string shortDescription, string descripton)
+        {
+            ChangeProgrammName(title);
+            ChangeShortDescription(shortDescription);
+            ChangeDescription(descripton);
+            return this;
+        }
     }
 }
